a64c5b81817d2328fab7635bb5ca2c7100b0d227
Wilbert Sible <wilbertsible@gmail.com>
2020-06-22 23:23:50 -0700

    Added customer feedback

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,24 +12,23 @@ export const About = () => {
                 <img src={profile} alt = "" className="profile-photo"></img>
             </div>
             <p>
-                Wilbert Sible is a student currently taking his Master’s Degree in Computer Science and Software 
-                Engineering at the University of Washington Bothell. His fields of interest are in Software Management, 
-                Artificial Intelligence, Big Data Analytics, and the Internet of Things. 
-                He loves to solve puzzles and to tinker with things and see how they work.
+                Wilbert Sible is currently taking his Master’s Degree in Computer Science and Software 
+                Engineering at the University of Washington Bothell. He is interested in software management, 
+                artificial intelligence, big data analytics, and the internet of things. 
+                He loves to solve puzzles and to tinker with things, and to see how they work.
             </p>
             <br></br>
             <p>     
                 While working on his degree, he tutors college students at the Quantitative Skills Center at the University 
-                of Washington Bothell and teaches kids game development using Scratch and CodeCombat at Mighty Coders. 
-                He believes that through teaching, he will fill the gaps in his own knowledge. He cherishes the moments 
-                when the students get that “eureka” moment where everything just clicks in their heads because, at that moment, 
-                he has made an impact in their lives.
+                of Washington Bothell, and teaches kids game development using Scratch and CodeCombat at Mighty Coders. 
+                Through teaching, he can fill the gaps in his technical knowledge. He cherishes the times 
+                when his students learn and get a “eureka” moment as he knows he made an impact.
             </p>
             <br></br>
             <p> 
-                Before graduate school, Wilbert worked as an environmental chemist analyzing hazardous waste in water and soil samples in 
-                Las Vegas, Nevada. He is the guy to call when something is amiss with anything in the lab. He assisted in all the duties 
-                in the lab, including processing data coming from other labs, logging-in samples, generating reported data and a lot more.
+                Before graduate school, Wilbert worked as an environmental chemist, analyzing hazardous waste in water and soil samples.
+                He assisted in all the duties in the lab, including generating and processing data from other labs, and logging samples.
+                He was the go-to guy when something was amiss in the lab.
             </p>
             <br></br>
             <p>
@@ -38,8 +37,8 @@ export const About = () => {
             </p>
             <br></br>
             <p>
-                In his spare time, Wilbert plays Ultimate Frisbee, goes to trivia nights, and plays board games with friends and family. 
-                He enjoys going out with friends to just get drinks and listen to everybody’s day-to-day adventures.  
+                In his spare time, Wilbert plays Ultimate Frisbee, goes to trivia nights, and plays board games. 
+                He also enjoys hanging out with his friends and family.  
             </p>
             <Link to="/About/Resume" className="button">View Resume</Link>
         </div>
